4115079b17599d2c82eab4013c8b6c49a0980435
alexey-cuttle <aleksey.p@cuttlesystems.com>
2023-06-08 18:50:51 +0600

    add fast_api_models: Filmwork, Person, Genre

---
+++ app/app/schemas/persons.py
@@ -0,0 +1,16 @@
+from typing import Optional, List
+
+import orjson
+from pydantic import BaseModel
+
+from app.schemas.mixins import orjson_dumps
+
+
+class Person(BaseModel):
+    id: str
+    full_name: str
+    films: Optional[List[dict]]
+
+    class Config:
+        json_loads = orjson.loads
+        json_dumps = orjson_dumps
