20caabb53733144a8ec7dd94e43f1e3a361b646e
Liudmila Komarovskikh <lucy.komar@gmail.com>
2023-06-09 15:59:57 +0100

    fixing persons

--- app/app/api/v1/endpoints/persons.py
+++ app/app/api/v1/endpoints/persons.py
@@ -3,7 +3,7 @@ from fastapi import APIRouter, Depends
 from typing import List, Optional
 
 from app.schemas.persons import Person
-from app.schemas.films import FilmWork
+from app.schemas.films import FilmWork, FilmWorkPerson
 from app.services.person import person_service, PersonService
 
 router = APIRouter()
@@ -22,5 +22,5 @@ async def list_films(
         person_id: str,
         rating_order: Optional[str] = None,
         person_service: PersonService = Depends(person_service)
-) -> List[FilmWork]:
+) -> List[FilmWorkPerson]:
     return await person_service.list(person_id=person_id, rating_order=rating_order)
--- app/app/services/person.py
+++ app/app/services/person.py
@@ -12,7 +12,7 @@ from app.db.init_redis import get_redis
 from app.db.init_etl import get_elastic
 
 from app.schemas.persons import Person
-from app.schemas.films import FilmWork
+from app.schemas.films import FilmWork, FilmWorkPerson
 
 from app.exceptions.person_exception import NotFoundPerson
 from app.exceptions.film_exception import NotFoundFilm
@@ -34,10 +34,12 @@ class PersonService:
 
     async def list(
             self,
-            person_id: str
+            person_id: str,
+            rating_order: Optional[str] = None
     ) -> Optional[List[FilmWork]]:
         list_films = await self._get_films_by_person_id(
-            person_id=person_id
+            person_id=person_id,
+            rating_order=rating_order
         )
         return list_films
 
@@ -50,11 +52,17 @@ class PersonService:
 
     async def _get_films_by_person_id(self, person_id: str,
                                       rating_order: Optional[str] = None) -> \
-            Optional[List[FilmWork]]:
+            Optional[List[FilmWorkPerson]]:
+        person = await self._get_person_from_etl(person_id=person_id)
+        # film_ids = [film['id'] for films in person.films for film in films]
+        film_ids = []
+        for film in person.films:
+            film_ids.append(film['id'])
+        print(film_ids)
 
         body = {
             "query": {
-                "match": {"person_id": person_id}
+                "terms": {"id": film_ids}
             }
         }
 
@@ -71,7 +79,7 @@ class PersonService:
             response = await self._es.search(index='movies', body=body)
         except NotFoundError:
             raise NotFoundFilm
-        return [FilmWork(**doc['_source']) for doc in response['hits']['hits']]
+        return [FilmWorkPerson(**doc['_source']) for doc in response['hits']['hits']]
 
 
 @lru_cache()
