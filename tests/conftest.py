71f111fafd74f5967c59216415f4fef3ba7f419f
Liudmila Komarovskikh <lucy.komar@gmail.com>
2023-06-26 23:41:24 +0100

    adding more tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,36 +2,52 @@ import json
 from typing import List
 
 import pytest
-import asyncio
+import aiohttp
 
 from elasticsearch import AsyncElasticsearch
 
 from settings import test_settings
 
+
 @pytest.fixture(scope='session')
 async def es_client():
-    client = AsyncElasticsearch(hosts='127.0.0.1:9200')
+    hosts = f'{test_settings.es_schema}://{test_settings.es_host}:{test_settings.es_port}'
+    client = AsyncElasticsearch(hosts=hosts)
     yield client
     await client.close()
 
+
 @pytest.fixture
-def es_write_data():
-    async def inner(data: List[dict]):
-        hosts = f'{test_settings.es_schema}://{test_settings.es_host}:{test_settings.es_port}'
+def es_write_data(es_client: AsyncElasticsearch):
+    async def inner(data: List[dict], index: str):
         bulk_query = []
         for row in data:
             bulk_query.extend([
-                json.dumps({'index': {'_index': 'movies', '_id': row['id']}}),
+                json.dumps({'index': {'_index': index, '_id': row['id']}}),
                 json.dumps(row)
             ])
 
         str_query = '\n'.join(bulk_query) + '\n'
 
-        es_client = AsyncElasticsearch(hosts=hosts)
         response = await es_client.bulk(operations=str_query, refresh=True)
-        await es_client.close()
         if response['errors']:
-            print(response)
             raise Exception('Ошибка записи данных в Elasticsearch')
+
+    return inner
+
+
+@pytest.fixture
+async def make_get_request():
+    async def inner(url: str, params: dict):
+        session = aiohttp.ClientSession()
+
+        try:
+            async with session.get(url, params=params) as response:
+                data = await response.json()
+                status = response.status
+        finally:
+            await session.close()
+
+        return data, status
 
     return inner
--- tests/functional/src/test_search.py
+++ tests/functional/src/test_search.py
@@ -1,6 +1,5 @@
 import uuid
 
-import aiohttp
 import pytest
 
 
@@ -8,17 +7,28 @@ import pytest
     'query_data, expected_answer',
     [
         (
-                {'query': 'The Star', "page_size": 50},
+                {'query': 'The Star', "page_size": 100},
                 {'status': 200, 'length': 50}
         ),
         (
                 {'query': ''},
                 {'status': 200, 'length': 0}
+        ),
+        (
+                {'query': 'Action', "page_size": 10},
+                {'status': 200, 'length': 10}
+        ),
+        (
+                {'query': 'Sci-Fi', "page_size": 20},
+                {'status': 200, 'length': 20}
+        ),
+        (
+                {'query': 'Non-existent', "page_size": 100},
+                {'status': 200, 'length': 0}
         )
     ]
 )
-@pytest.mark.asyncio
-async def test_search(es_write_data, query_data, expected_answer):
+async def test_search(make_get_request, es_write_data, query_data: dict, expected_answer: dict):
     es_data = [{
         'id': str(uuid.uuid4()),
         'imdb_rating': 8.5,
@@ -37,17 +47,9 @@ async def test_search(es_write_data, query_data, expected_answer):
             {'id': '444', 'name': 'Howard'}
         ]
     } for _ in range(50)]
-
     await es_write_data(es_data)
-
-    session = aiohttp.ClientSession()
     url = 'http://movies_app_api/api/v1/films/search'
-    print(url)
-    async with session.get(url, params=query_data) as response:
-        data = await response.json()
-        status = response.status
-    await session.close()
-
+    data, status = await make_get_request(url, query_data)
     assert status == expected_answer['status']
     assert len(data) == expected_answer['length']
 
