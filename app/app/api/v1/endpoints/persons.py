99f26eb5116a95e7b1f7a1b95820d61abf689f9d
Liudmila Komarovskikh <lucy.komar@gmail.com>
2023-06-29 21:51:51 +0100

    adding commons

--- app/app/api/v1/endpoints/persons.py
+++ app/app/api/v1/endpoints/persons.py
@@ -2,10 +2,12 @@ from fastapi import APIRouter, Depends
 
 from typing import List
 
+from app.core.commons import PaginateQueryParams
 from app.core.config import settings
 from app.schemas.persons import Person
 from app.schemas.films import FilmWorkShort
 from app.services.person import get_person_service, PersonService
+
 
 router = APIRouter()
 
@@ -27,31 +29,27 @@ async def get_person(
             description='Searches for a person by id and returns truncated information '
                         'about films in which the person was involved.',
             )
-async def get_persons_films_by_id(
+async def get_persons_films_by_id(commons: Annotated[PaginateQueryParams, Depends(PaginateQueryParams)],
         person_id: str,
         rating_order: str | None = None,
-        page: int | None = 1,
-        page_size: int | None = settings.default_page_size,
         person_service: PersonService = Depends(get_person_service)
 ) -> List[FilmWorkShort] | None:
 
     return await person_service.list(
         person_id=person_id,
         rating_order=rating_order,
-        page=page,
-        page_size=page_size
+        page=commons.page,
+        page_size=commons.page_size
     )
 
 
 @router.get('/search')
-async def search_persons(
+async def search_persons(commons: Annotated[PaginateQueryParams, Depends(PaginateQueryParams)],
         query: str,
-        page: int | None = 1,
-        page_size: int | None = settings.default_page_size,
         person_service: PersonService = Depends(get_person_service)
 ) -> List[Person]:
     return await person_service.search(
         query=query,
-        page=page,
-        page_size=page_size
+        page=commons.page,
+        page_size=commons.page_size
     )
---
+++ app/app/core/commons.py
@@ -0,0 +1,25 @@
+from fastapi import Query
+
+from app.core.config import settings
+
+class PaginateQueryParams:
+    """Dependency class to parse pagination query params."""
+
+    def __init__(
+        self,
+        page_number: int = Query(
+            1,
+            title="Page number",
+            description="Page number to return",
+            ge=1,
+        ),
+        page_size: int = Query(
+            settings.default_page_size,
+            title="Size of page.",
+            description="The number of records returned per page",
+            ge=1,
+            le=500,
+        ),
+    ):
+        self.page_number = page_number
+        self.page_size = page_size
