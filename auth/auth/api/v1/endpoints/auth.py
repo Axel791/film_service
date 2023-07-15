be12a6dae8043856940e4a80941943949dd9f368
Liudmila Komarovskikh <lucy.komar@gmail.com>
2023-07-15 12:46:04 +0100

    new changes

--- auth/auth/api/v1/endpoints/auth.py
+++ auth/auth/api/v1/endpoints/auth.py
@@ -1,21 +1,36 @@
-from fastapi import APIRouter, Depends
-from auth.schemas import Token, UserCreate, UserInDB
-from auth.services import AuthService
+db = SessionLocal()
+redis_client = Redis(host='localhost', port=6379, password='your_redis_password')
+auth_service = AuthService(db, redis_client)
 
-router = APIRouter()
-auth_service = AuthService()
 
 @router.post("/register", response_model=UserInDB)
 def register(user_data: UserCreate) -> UserInDB:
     user = auth_service.create_user(user_data)
     return user
 
+
 @router.post("/login", response_model=Token)
 def login(username: str, password: str) -> Token:
     user = auth_service.authenticate_user(username, password)
     access_token = auth_service.create_access_token(user)
+
+    # Store the access token in Redis
+    redis_client.set(username, access_token.access_token)
+
     return access_token
 
-@router.get("/user/me", response_model=UserInDB)
-def get_current_user(current_user: UserInDB = Depends(auth_service.get_current_user)):
-    return current_user
+
+@router.get("/protected")
+def protected_route(current_user: UserInDB = Depends(auth_service.verify_access_token)):
+    return {"message": "Protected Route"}
+
+
+@router.post("/refresh-token", response_model=Token)
+def refresh_token(refresh_token: str) -> Token:
+    user = auth_service.verify_refresh_token(refresh_token)
+    access_token = auth_service.create_access_token(user)
+
+    # Store the new access token in Redis
+    redis_client.set(user.username, access_token.access_token)
+
+    return access_token
--- auth/auth/services/auth.py
+++ auth/auth/services/auth.py
@@ -1,61 +1,94 @@
-from fastapi import HTTPException, status
-from passlib.context import CryptContext
+from fastapi import APIRouter, Depends, HTTPException
 from datetime import datetime, timedelta
 from jose import jwt
+from redis import Redis
+from sqlalchemy.orm import Session
 
 from auth.core.config import settings
 from auth.models import User
-from auth.db import session
-from auth.schemas import Token, TokenPayload, UserCreate, UserInDB
+from auth.db import SessionLocal
+from auth.schemas import Token, UserCreate, UserInDB
 
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
-
+router = APIRouter()
 
 class AuthService:
+    def __init__(self, db: Session, redis_client: Redis):
+        self.db = db
+        self.redis_client = redis_client
+
     def authenticate_user(self, username: str, password: str) -> UserInDB:
-        user = session.query(User).filter(User.username == username).first()
-        if not user or not pwd_context.verify(password, user.password):
+        user = self.db.query(User).filter(User.username == username).first()
+        if not user or not user.verify_password(password):
             raise HTTPException(
-                status_code=status.HTTP_401_UNAUTHORIZED,
+                status_code=401,
                 detail="Incorrect username or password",
                 headers={"WWW-Authenticate": "Bearer"},
             )
         return UserInDB.from_orm(user)
 
     def create_access_token(self, user: UserInDB) -> Token:
-        expiry = datetime.utcnow() + timedelta(minutes=settings.access_token_expire_minutes)
+        expiry = datetime.utcnow() + timedelta(minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES)
         payload = {
             "sub": str(user.id),
             "username": user.username,
             "exp": expiry,
         }
-        token = jwt.encode(payload, settings.jwt_secret_key, algorithm=settings.algorithm)
+        token = jwt.encode(payload, settings.SECRET_KEY, algorithm=settings.ALGORITHM)
         return Token(access_token=token, token_type="bearer")
 
-    def create_user(self, user_data: UserCreate) -> UserInDB:
-        user = User(
-            username=user_data.username,
-            password=self.hash_password(user_data.password),
-        )
-        session.add(user)
-        session.commit()
-        session.refresh(user)
-        return UserInDB.from_orm(user)
-
-    def hash_password(self, password: str) -> str:
-        return pwd_context.hash(password)
-
-    def decode_token(self, token: str) -> TokenPayload:
+    def verify_access_token(self, access_token: str) -> UserInDB:
         try:
             payload = jwt.decode(
-                token,
+                access_token,
                 settings.SECRET_KEY,
-                algorithms=[settings.algorithm],
+                algorithms=[settings.ALGORITHM],
             )
-            return TokenPayload(**payload)
+            user_id = payload["sub"]
+            username = payload["username"]
+
+            # Verify if the access token matches the one stored in Redis
+            stored_access_token = self.redis_client.get(username)
+            if stored_access_token != access_token.encode():
+                raise HTTPException(
+                    status_code=401,
+                    detail="Invalid access token",
+                    headers={"WWW-Authenticate": "Bearer"},
+                )
+
+            # Retrieve the user from the database
+            user = self.db.query(User).get(user_id)
+            if not user:
+                raise HTTPException(
+                    status_code=401,
+                    detail="Invalid user",
+                    headers={"WWW-Authenticate": "Bearer"},
+                )
+
+            return UserInDB.from_orm(user)
+
         except jwt.JWTError:
             raise HTTPException(
-                status_code=status.HTTP_401_UNAUTHORIZED,
-                detail="Invalid token",
+                status_code=401,
+                detail="Invalid access token",
                 headers={"WWW-Authenticate": "Bearer"},
             )
+
+    def verify_refresh_token(self, refresh_token: str) -> UserInDB:
+        user_id = self.redis_client.get(refresh_token)
+        if not user_id:
+            raise HTTPException(
+                status_code=401,
+                detail="Invalid refresh token",
+                headers={"WWW-Authenticate": "Bearer"},
+            )
+        user = self.db.query(User).get(user_id)
+        if not user:
+            raise HTTPException(
+                status_code=401,
+                detail="Invalid user",
+                headers={"WWW-Authenticate": "Bearer"},
+            )
+        return UserInDB.from_orm(user)
+
+
+
