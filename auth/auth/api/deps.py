608346b386f77bcc4d9e66d191c9f9fd0b7a5efb
Liudmila Komarovskikh <lucy.komar@gmail.com>
2023-07-22 16:16:22 +0100

    api login history endpoint

--- auth/auth/api/deps.py
+++ auth/auth/api/deps.py
@@ -1,11 +1,47 @@
 from functools import wraps
 from uuid import uuid4
 
-from fastapi import Depends
 from dependency_injector.wiring import inject, Provide
+from fastapi import Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer
+
+from jose import JWTError, jwt
 
 from auth.core.containers import Container
 from auth.db.session import scope
+from auth.core.config import settings
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/auth/api/v1/auth/login")
+
+SECRET_KEY = settings.JWT_REFRESH_SECRET_KEY
+
+
+@inject
+async def get_current_user(
+        token: str = Depends(oauth2_scheme),
+        rep_user=Depends(Provide[Container.repository_user])
+):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=["HS256"])
+        username: str = payload.get("email")
+        if username is None:
+            raise credentials_exception
+    except JWTError:
+        raise credentials_exception
+    user = rep_user.get(email=username)
+    if user is None:
+        raise credentials_exception
+    return user
+
+
+@inject
+async def create_session():
+    scope.set(str(uuid4()))
 
 
 @inject
