a19a762a0311a37f3a50f1c8b4d611cb929b93ad
Kanat <kanatseytov@yandex.com>
2023-07-26 20:40:05 +0300

    jaeger

--- auth/auth/main.py
+++ auth/auth/main.py
@@ -6,7 +6,29 @@ from auth.core.config import settings
 from auth.core.containers import Container
 
 from fastapi.middleware.cors import CORSMiddleware
-from fastapi import FastAPI
+from fastapi import FastAPI, Request, status
+from fastapi.responses import ORJSONResponse
+from opentelemetry import trace
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.instrumentation.fastapi import FastAPIInstrumentor
+from opentelemetry.sdk.trace.export import BatchSpanProcessor, ConsoleSpanExporter
+from opentelemetry.exporter.jaeger.thrift import JaegerExporter
+
+
+def configure_tracer() -> None:
+    trace.set_tracer_provider(TracerProvider())
+    trace.get_tracer_provider().add_span_processor(
+        BatchSpanProcessor(
+            JaegerExporter(
+                agent_host_name=settings.JAEGER_HOST,
+                agent_port=settings.JAEGER_PORT,
+            )
+        )
+    )
+    trace.get_tracer_provider().add_span_processor(BatchSpanProcessor(ConsoleSpanExporter()))
+
+
+configure_tracer()
 
 
 def create_app():
@@ -33,3 +55,14 @@ def create_app():
 
 
 app = create_app()
+FastAPIInstrumentor.instrument_app(app)
+
+
+@app.middleware('http')
+async def before_request(request: Request, call_next):
+    response = await call_next(request)
+    request_id = request.headers.get('X-Request-Id')
+    if not request_id:
+        return ORJSONResponse(status_code=status.HTTP_400_BAD_REQUEST,
+                              content={'detail': 'X-Request-Id is required'})
+    return response
